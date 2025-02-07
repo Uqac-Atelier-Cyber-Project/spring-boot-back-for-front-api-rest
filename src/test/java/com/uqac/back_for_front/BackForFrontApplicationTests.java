fc476b05c6916a39d8718e1490aa0adc644c2203
Aubry TONNERRE <47123188+kiurow590@users.noreply.github.com>
2025-02-07 03:26:29 +0000

    Add File Project

---
+++ src/test/java/com/uqac/back_for_front/BackForFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqac.back_for_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackForFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
