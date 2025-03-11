b363479358970653686e4d608e10c529adec6501
fra-2107 <timchimienti@gmail.com>
2025-03-11 13:31:29 -0400

    mapping datacontroller

--- src/main/java/com/uqac/back_for_front/models/User.java
+++ src/main/java/com/uqac/back_for_front/models/User.java
@@ -6,30 +6,21 @@ import jakarta.persistence.Id;
 @Entity
 public class User {
     @Id
-    private String id;  // Identifiant unique de l'utilisateur
-    
-    private String email;  // Email de l'utilisateur
+    private String email;  // Identifiant unique de l'utilisateur
+
     private String password;  // Mot de passe
 
     // Getters et Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setPassword(String password) {
